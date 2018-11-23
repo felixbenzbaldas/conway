77a80599aef69e88e4438056fc5d603e07fa0ebc
Felix Benz-Baldas <f.benz-baldas@mailbox.org>
2018-11-23 17:16:58 +0100

    add survive

--- src/test/java/gof/TestApp.java
+++ src/test/java/gof/TestApp.java
@@ -6,13 +6,21 @@ import org.junit.Test;
 
 public class TestApp {
 	
-	
+
 	@Test
 	public void underpopulation() throws Exception {
 		assertEquals("0", calculate("0000X0000"));
 	}
 	
+	@Test
+	public void survive() throws Exception {
+		assertEquals("X", calculate("000XXX000"));
+	}
+	
 	private String calculate(String board) {
+		if (board.replace("0", "").length() > 2) {
+			return "X";
+		}
 		return "0";
 	}
 	
