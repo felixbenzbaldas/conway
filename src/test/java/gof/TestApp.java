c923dbfb904a5827cb0ef2f621fd381a12df2f3e
Felix Benz-Baldas <f.benz-baldas@mailbox.org>
2018-11-23 20:43:28 +0100

    improve tests

--- src/test/java/gof/TestApp.java
+++ src/test/java/gof/TestApp.java
@@ -34,13 +34,25 @@ public class TestApp {
 	
 	@Test
 	public void overpopulation() throws Exception {
-		assertEquals(new HashSet<Point>(), new App().calculate("0X0XXX0X0"));
+		Set<Point> livingCells = new HashSet<Point>();
+		livingCells.add(new Point(0, 1));
+		livingCells.add(new Point(1, 0));
+		livingCells.add(new Point(1, 1));
+		livingCells.add(new Point(1, 2));
+		livingCells.add(new Point(2, 1));
+		
+		assertEquals(new HashSet<Point>(), new App().calculate("0X0XXX0X0", livingCells));
 	}
 	
 	
 	@Test
 	public void birth() throws Exception {
-		assertEquals(boardWithLivingCell, new App().calculate("0X0X0X000"));
+		Set<Point> livingCells = new HashSet<Point>();
+		livingCells.add(new Point(0, 1));
+		livingCells.add(new Point(1, 0));
+		livingCells.add(new Point(1, 2));
+		
+		assertEquals(boardWithLivingCell, new App().calculate("0X0X0X000", livingCells));
 	}
 	
 }
