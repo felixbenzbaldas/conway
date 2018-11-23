b61099600e4bca70a74fb8783272e8b0907d3f04
Felix Benz-Baldas <f.benz-baldas@mailbox.org>
2018-11-23 17:10:17 +0100

    improve test

--- src/test/java/gof/TestApp.java
+++ src/test/java/gof/TestApp.java
@@ -5,12 +5,16 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TestApp {
+	
+	
 	@Test
 	public void underpopulation() throws Exception {
-		assertEquals("0", calculate("X"));
+		assertEquals("0", calculate("0000X0000"));
 	}
 	
 	private String calculate(String board) {
 		return "0";
 	}
+	
+	
 }
