48c25321c01070a7e584f2129ad06565d8831e07
Felix Benz-Baldas <f.benz-baldas@mailbox.org>
2018-11-23 17:08:55 +0100

    add underpopulation

--- src/test/java/gof/TestApp.java
+++ src/test/java/gof/TestApp.java
@@ -1,10 +1,16 @@
 package gof;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class TestApp {
 	@Test
-	public void testInstallation() throws Exception {
-		
+	public void underpopulation() throws Exception {
+		assertEquals("0", calculate("X"));
+	}
+	
+	private String calculate(String board) {
+		return "0";
 	}
 }
