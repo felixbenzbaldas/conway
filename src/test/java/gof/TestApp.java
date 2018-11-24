e75d52b5919f2580111a3bca49f5b20abe55da03
Felix Benz-Baldas <f.benz-baldas@mailbox.org>
2018-11-24 17:48:08 +0100

    improve tests

--- src/test/java/gof/TestApp.java
+++ src/test/java/gof/TestApp.java
@@ -1,6 +1,7 @@
 package gof;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -18,7 +19,10 @@ public class TestApp {
 	public void underpopulation() throws Exception {
 		Set<Point> livingCells = new HashSet<Point>();
 		livingCells.add(new Point(1, 1));
-		assertEquals(new HashSet<Point>(), new App().calculate(livingCells));
+		
+		Set<Point> livingCells_nextGeneration = new HashSet<Point>();
+		
+		assertEquals(livingCells_nextGeneration, new App().calculate(livingCells));
 	}
 	
 	@Test
@@ -27,8 +31,13 @@ public class TestApp {
 		livingCells.add(new Point(1, 0));
 		livingCells.add(new Point(1, 1));
 		livingCells.add(new Point(1, 2));
+
+		Set<Point> livingCells_nextGeneration = new HashSet<Point>();
+//		livingCells_nextGeneration.add(new Point(0, 1));
+		livingCells_nextGeneration.add(new Point(1, 1));
+//		livingCells_nextGeneration.add(new Point(2, 1));
 		
-		assertEquals(boardWithLivingCell, new App().calculate(livingCells));
+		assertEquals(livingCells_nextGeneration, new App().calculate(livingCells));
 	}
 	
 	@Test
@@ -40,7 +49,7 @@ public class TestApp {
 		livingCells.add(new Point(1, 2));
 		livingCells.add(new Point(2, 1));
 		
-		assertEquals(new HashSet<Point>(), new App().calculate(livingCells));
+		assertTrue(!new App().calculate(livingCells).contains(new Point(1,1)));
 	}
 	
 	
@@ -51,6 +60,6 @@ public class TestApp {
 		livingCells.add(new Point(1, 0));
 		livingCells.add(new Point(1, 2));
 		
-		assertEquals(boardWithLivingCell, new App().calculate(livingCells));
+		assertTrue(new App().calculate(livingCells).contains(new Point(1,1)));
 	}
 }
