1612149804621be567866e3dc0ef2a49cc5ac63a
Felix Benz-Baldas <f.benz-baldas@mailbox.org>
2018-12-26 09:31:49 +0100

    print board

--- src/main/java/gol/Runner.java
+++ src/main/java/gol/Runner.java
@@ -4,18 +4,30 @@ import java.util.concurrent.TimeUnit;
 
 public class Runner {
 	
+	private Board board;
+	
 	public static void main(String[] args) throws InterruptedException {
 		new Runner().run();
 	}
 	
 	private void run() throws InterruptedException {
-		Board board = new Board();
+		board = new Board();
 		board.setAlive(1, 1);
 		
 		while (true) {
-			System.out.println(board.isAlive(1, 1));
-			TimeUnit.MILLISECONDS.sleep(100);
+			printBoard();
+			TimeUnit.MILLISECONDS.sleep(1000);
 			board.stepToNextGeneration();
+		}
+	}
+	
+	private void printBoard() {
+		System.out.println("=============================================================");
+		for (int row = 0; row < 20; row++) {
+			for (int column = 0; column < 20; column++) {
+				System.out.print((board.isAlive(row, column)) ? " X " : " - ");
+			}
+			System.out.println("");
 		}
 	}
 }
