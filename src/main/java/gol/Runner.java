67a0acf5b4c316f3f2a202de6d2b902c346ab920
Felix Benz-Baldas <f.benz-baldas@mailbox.org>
2018-12-26 08:55:40 +0100

    add runner

---
+++ src/main/java/gol/Runner.java
@@ -0,0 +1,12 @@
+package gol;
+
+import java.util.concurrent.TimeUnit;
+
+public class Runner {
+	public static void main(String[] args) throws InterruptedException {
+		while (true) {
+			System.out.println("[Board]");
+			TimeUnit.MILLISECONDS.sleep(100);
+		}
+	}
+}
